ff41a655dcf5cd3c9e86fa861be1e0dbea58cf4a
Tobias Vrolijk <vrolijktobias@gmail.com>
2023-11-20 19:23:54 +0100

    push

--- i-love-web-app/src/routes/+page.svelte
+++ i-love-web-app/src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+    
+</body>
+<main>
+    <article class="page1">
+        <a href="page1-link"> </a>
+
+    </article>
+    <article class="main-folder">
+
+    </article>
+
+</main>
+
+<!-- css --><!-- css --><!-- css --><!-- css --><!-- css --><!-- css --><!-- css --><!-- css --><!-- css -->
+
+<style>
+
+body{
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    flex-direction: row-reverse;
+}
+
+
+.main-folder{
+    width: 50em;
+    height: 30em;
+    background-color: #DEC292;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    border-radius: 10px;
+    z-index: 100;
+}
+
+.page1{
+    width: 4em;
+    height: 2em;
+    background-color: #DEC292;
+    border-radius: 10px;
+}
+
+
+</style>
